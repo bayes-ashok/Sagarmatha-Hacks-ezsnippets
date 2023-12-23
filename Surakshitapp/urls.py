2fe033964b5d228aeac00c48eca2acf4fe2c5013
Ashok Shah <121223930+bayes-ashok@users.noreply.github.com>
2023-12-23 11:21:33 +0545

    basic templating done with template inheritance

--- Surakshitapp/urls.py
+++ Surakshitapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    
+    path('dashboard', views.dashboard,name='dashboard'),
+    path('alerts', views.alerts,name='alerts'),
+    path('earthquake', views.earthquake,name='earthquake'),
+    path('flood', views.flood,name='flood'),
+    path('landslide', views.landslide,name='landslide'),
+    path('glof', views.glof,name='glof'),
 ]
--- Surakshitapp/views.py
+++ Surakshitapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def alerts(request):
+    return render(request,'alerts.html')
+
+def earthquake(request):
+    return render(request,'earthquake.html')
+
+def flood(request):
+    return render(request,'flood.html')
+
+def glof(request):
+    return render(request,'glof.html')
+
+def landslide(request):
+    return render(request,'landslide.html')
